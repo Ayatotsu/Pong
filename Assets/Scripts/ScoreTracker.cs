2d20a1ca49eba6761f9656f57beed4db9f768e5a
Edgardo Merelos Jr <edgardomerelosjr@gmail.com>
2024-02-04 21:13:28 +0800

    spaces

--- Assets/Scripts/ScoreTracker.cs
+++ Assets/Scripts/ScoreTracker.cs
@@ -73,8 +73,6 @@ public class ScoreTracker : MonoBehaviour
                         ButtonMainMenu.gameObject.SetActive(true);
                     }
                     break;
-
-
             }
         }
     }
